7783b04691f4a360fd12ff7193534f440898d217
ManiCruz <promisecruz11@gmail.com>
2024-05-30 08:49:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -76,8 +76,8 @@ module.exports = {
  
   author : process.env.PACK_AUTHER|| "",
   packname: process.env.PACK_NAME || "",
-  botname : process.env.BOT_NAME  || "sᴜʜᴀɪʟ-ᴍᴅ",
-  ownername:process.env.OWNER_NAME|| "It'x Suhail",
+  botname : process.env.BOT_NAME  || "knight bot",
+  ownername:process.env.OWNER_NAME|| "kimg cruz",
 
 
   errorChat : process.env.ERROR_CHAT || "",
